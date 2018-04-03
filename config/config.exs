c3aab01d5fcbd0211d42433a07b02bb179219a78
laksamanakeris <kerismelayu@gmail.com>
2018-04-03 09:38:26 +0800

    Initial commit

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :bijakhq,
+  ecto_repos: [Bijakhq.Repo]
+
+# Configures the endpoint
+config :bijakhq, BijakhqWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "oAdfNSfyb1IA5os5QiWtlZ7npPCnuQZyk+nG8K2Xx3oDcjCW9eDrh3XGPANm77RS",
+  render_errors: [view: BijakhqWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Bijakhq.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
---
+++ test/bijakhq_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule BijakhqWeb.PageControllerTest do
+  use BijakhqWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
