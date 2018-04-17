4befd477493a16c4563ab48cef04ed555701b1b0
laksamanakeris <kerismelayu@gmail.com>
2018-04-18 07:15:53 +0800

    Added swagger

--- lib/bijakhq_web/router.ex
+++ lib/bijakhq_web/router.ex
@@ -1,6 +1,14 @@
 defmodule BijakhqWeb.Router do
   use BijakhqWeb, :router
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
     plug Phauxth.Authenticate, method: :token
@@ -14,6 +22,28 @@ defmodule BijakhqWeb.Router do
     get "/confirm", ConfirmController, :index
     post "/password_resets", PasswordResetController, :create
     put "/password_resets/update", PasswordResetController, :update
+  end
+
+  scope "/", BijakhqWeb do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  def swagger_info do
+    %{
+      info: %{
+        version: "1.0",
+        title: "Bijak HQ"
+      }
+    }
+  end
+
+  scope "/swagger" do
+    forward "/", PhoenixSwagger.Plug.SwaggerUI,
+      otp_app: :bijakhq,
+      swagger_file: "swagger.json",
+      disable_validator: true
   end
 
 end
