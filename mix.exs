7dae70b1972c70bbbfcbc5987bf7897c46e440cb
laksamanakeris <kerismelayu@gmail.com>
2018-04-18 14:09:22 +0800

    Removes swagger

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Bijakhq.Mixfile do
       version: "0.0.1",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix, :gettext, :phoenix_swagger] ++ Mix.compilers,
+      compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
       aliases: aliases(),
       deps: deps()
