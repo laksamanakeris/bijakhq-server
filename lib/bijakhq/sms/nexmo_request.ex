7c5450cc684178ec6dfa09e5b3bdfb09c78a640d
laksamanakeris <kerismelayu@gmail.com>
2018-05-17 04:49:00 +0800

    Change column name

--- lib/bijakhq/sms/nexmo_request.ex
+++ lib/bijakhq/sms/nexmo_request.ex
@@ -7,7 +7,7 @@ defmodule Bijakhq.Sms.NexmoRequest do
     field :completed_at, :date
     field :is_completed, :boolean, default: false
     field :phone, :string
-    field :verification_id, :string
+    field :request_id, :string
     field :verified_at, :date
 
     timestamps()
@@ -16,7 +16,13 @@ defmodule Bijakhq.Sms.NexmoRequest do
   @doc false
   def changeset(%NexmoRequest{} = nexmo_request, attrs) do
     nexmo_request
-    |> cast(attrs, [:phone, :verification_id, :is_completed, :verified_at, :completed_at])
-    |> validate_required([:phone, :verification_id, :is_completed, :verified_at, :completed_at])
+    |> cast(attrs, [:phone, :request_id, :is_completed, :verified_at, :completed_at])
+    |> validate_required([:phone, :request_id, :is_completed, :verified_at, :completed_at])
+  end
+
+  def create_changeset(%NexmoRequest{} = nexmo_request, attrs) do
+    nexmo_request
+    |> cast(attrs, [:phone, :request_id])
+    |> validate_required([:phone, :request_id])
   end
 end
