a6bd42f025c9bc79259941b02c43bad6cf76acea
laksamanakeris <kerismelayu@gmail.com>
2018-05-16 15:21:09 +0800

    Nexmo flow

--- lib/bijakhq_web/controllers/api/verification_controller.ex
+++ lib/bijakhq_web/controllers/api/verification_controller.ex
@@ -2,7 +2,8 @@ defmodule BijakhqWeb.Api.VerificationController do
   use BijakhqWeb, :controller
 
   import BijakhqWeb.Api.Authorize
-  alias Bijakhq.SMS.Nexmo
+  alias Bijakhq.Sms.Nexmo
+  alias Bijakhq.Sms.Verification
 
   plug :guest_check when action in [:verify]
 
@@ -12,10 +13,38 @@ defmodule BijakhqWeb.Api.VerificationController do
     IO.inspect params
     IO.puts "Phone number #{phone}"
 
-    response = Nexmo.authenticate(phone)
+    response = Verification.authenticate(phone)
+    IO.inspect response
+
+    process_verify(conn, phone, response);
+  end
+
+  def process_verify(conn, phone, {:ok, %{"status" => "0", "request_id" => request_id}}) do
+    render(conn, "verification.json", %{data: %{phone: phone, request_id: request_id}} )
+  end
+
+  def process_verify(conn, _, {:ok, %{"status" => _, "error_text" => error_text}}) do
+    render(conn, "error.json", error: error_text)
+  end
+
+  def process_verify(conn, _, {:error, _}) do
+    render(conn, "error.json", error: "Error sending out SMS")
+  end
+
+  def verify_request(conn, %{"request_id" => request_id, "code" => code} = params) do
+    IO.inspect request_id
+    IO.inspect params
+
+    response = Nexmo.verify(request_id, code)
 
     IO.inspect response
 
-    render(conn, "verification.json", phone: phone)
+    render(conn, "verify_result.json", %{data: %{request_id: request_id}} )
+  end
+
+  def cancel_request(conn, %{"request_id" => request_id} = params) do
+    IO.inspect params
+    IO.inspect request_id
+    render(conn, "verify_result.json", %{data: %{request_id: request_id}} )
   end
 end
---
+++ lib/bijakhq_web/views/api/verification_view.ex
@@ -0,0 +1,15 @@
+defmodule BijakhqWeb.Api.VerificationView do
+  use BijakhqWeb, :view
+
+  def render("error.json", %{error: message}) do
+    %{errors: %{details: message}}
+  end
+
+  def render("verification.json", %{data: %{phone: phone, request_id: request_id}}) do
+    %{phone: phone, request_id: request_id}
+  end
+
+  def render("verify_result.json", %{data: %{request_id: request_id}}) do
+    %{request_id: request_id}
+  end
+end
