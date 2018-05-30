7f2c3f3733b911bd1184bccb9dfc102026ac4c04
laksamanakeris <kerismelayu@gmail.com>
2018-05-30 14:39:26 +0800

    Added handling for game

--- lib/bijakhq_web/channels/game_session_channel.ex
+++ lib/bijakhq_web/channels/game_session_channel.ex
@@ -1,8 +1,13 @@
 defmodule BijakhqWeb.GameSessionChannel do
   use BijakhqWeb, :channel
 
+  alias BijakhqWeb.Presence
+
   def join("game_session:lobby", payload, socket) do
     if authorized?(payload) do
+
+      send(self(), :after_join)
+
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -22,8 +27,57 @@ defmodule BijakhqWeb.GameSessionChannel do
     {:noreply, socket}
   end
 
+  # GAME SESSION
+  def handle_in("game:start", payload, socket) do
+    broadcast socket, "game:start", payload
+    {:noreply, socket}
+  end
+
+  def handle_in("question:show", payload, socket) do
+    broadcast socket, "question:show", payload
+    {:noreply, socket}
+  end
+
+  def handle_in("question:end", payload, socket) do
+    broadcast socket, "question:end", payload
+    {:noreply, socket}
+  end
+
+  def handle_in("result:show", payload, socket) do
+    broadcast socket, "result:show", payload
+    {:noreply, socket}
+  end
+
+  def handle_in("result:end", payload, socket) do
+    broadcast socket, "result:end", payload
+    {:noreply, socket}
+  end
+
+  def handle_in("game:end", payload, socket) do
+    broadcast socket, "game:end", payload
+    {:noreply, socket}
+  end
+
+
+
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
   end
+
+  def handle_info(:after_join, socket) do
+    push socket, "presence_state", Presence.list(socket)
+
+    user = socket.assigns.user
+    IO.inspect socket
+
+    {:ok, _} = Presence.track(socket, "user:#{user.id}", %{
+      online_at: :os.system_time(:milli_seconds),
+      user_id: user.id,
+      username: user.username
+    })
+
+    {:noreply, socket}
+  end
+
 end
