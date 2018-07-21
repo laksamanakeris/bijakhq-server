615c448c8e1cc7b832e43ed207e6393851beb6e7
laksamanakeris <kerismelayu@gmail.com>
2018-07-21 20:11:12 +0800

    Rename modules

--- lib/bijakhq_web/views/api/quiz_session_view.ex
+++ lib/bijakhq_web/views/api/quiz_session_view.ex
@@ -35,10 +35,10 @@ defmodule BijakhqWeb.Api.QuizSessionView do
 
 
   def render("session_question.json", %{session_question: session_question}) do
-    %{data: render_many(session_question, BijakhqWeb.Api.GameQuestionView, "question.json")}
+    %{data: render_many(session_question, BijakhqWeb.Api.QuizGameQuestionView, "question.json")}
   end
 
   def render("session_question_show.json", %{session_question: session_question}) do
-    %{data: render_one(session_question, BijakhqWeb.Api.GameQuestionView, "question_preload.json")}
+    %{data: render_one(session_question, BijakhqWeb.Api.QuizGameQuestionView, "question_preload.json")}
   end
 end
