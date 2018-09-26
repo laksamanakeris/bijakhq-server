47a762e4a27eb0651e453b973188074835d363b4
laksamanakeris <kerismelayu@gmail.com>
2018-09-26 12:25:29 +0800

    Update payment table

--- priv/repo/migrations/20180926025529_update_payments_table_add_status.exs
+++ priv/repo/migrations/20180926025529_update_payments_table_add_status.exs
@@ -4,7 +4,12 @@ defmodule Bijakhq.Repo.Migrations.UpdatePaymentsTableAddStatus do
   def change do
 
     alter table(:payments) do
-      add :paymemt_status_id, references(:payment_statuses), default: 1
+      add :paymemt_status, references(:payment_statuses), default: 1
+      add :payment_type, references(:payment_types)
+      add :reference_id, :string
+      add :reference_details, :text
+      add :batch_paypal_email, :string  # Put it here for temporary
+      add :batch_paypal_id, :integer  # Put it here for temporary
     end
 
   end
