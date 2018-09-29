ce2936c901f9903661b471845cf4bc26e9d715f5
laksamanakeris <kerismelayu@gmail.com>
2018-09-29 14:47:07 +0800

    Payment request implementation

--- lib/bijakhq/payments/payment.ex
+++ lib/bijakhq/payments/payment.ex
@@ -3,6 +3,8 @@ defmodule Bijakhq.Payments.Payment do
   import Ecto.Changeset
 
   alias Bijakhq.Accounts.User
+  alias Bijakhq.Payments.PaymentType
+  alias Bijakhq.Payments.PaymentStatus
 
 
   schema "payments" do
@@ -12,6 +14,8 @@ defmodule Bijakhq.Payments.Payment do
 
     belongs_to :user, User, foreign_key: :user_id
     belongs_to :update_by, User, foreign_key: :updated_by
+    belongs_to :status, PaymentStatus, foreign_key: :payment_status
+    belongs_to :type, PaymentType, foreign_key: :payment_type
 
     timestamps()
   end
@@ -19,7 +23,7 @@ defmodule Bijakhq.Payments.Payment do
   @doc false
   def changeset(payment, attrs) do
     payment
-    |> cast(attrs, [:amount, :user_id, :payment_at, :remarks, :updated_by])
-    |> validate_required([:amount, :user_id, :updated_by])
+    |> cast(attrs, [:amount, :user_id, :payment_at, :remarks, :updated_by, :payment_status, :payment_type])
+    |> validate_required([:amount, :user_id, :updated_by, :payment_type ])
   end
 end
