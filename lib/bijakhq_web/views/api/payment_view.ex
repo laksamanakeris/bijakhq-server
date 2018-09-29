7255f93cd0e11d40cf388c2b09bc703d8a895a9b
laksamanakeris <kerismelayu@gmail.com>
2018-09-29 22:39:09 +0800

    Added payment type to payment view

--- lib/bijakhq_web/views/api/payment_view.ex
+++ lib/bijakhq_web/views/api/payment_view.ex
@@ -2,6 +2,8 @@ defmodule BijakhqWeb.Api.PaymentView do
   use BijakhqWeb, :view
   alias BijakhqWeb.Api.PaymentView
   alias BijakhqWeb.Api.UserView
+  alias BijakhqWeb.Api.PaymentStatusView
+  alias BijakhqWeb.Api.PaymentTypeView
 
   def render("index.json", %{payments: payments}) do
     %{data: render_many(payments, PaymentView, "payment.json")}
@@ -20,6 +22,10 @@ defmodule BijakhqWeb.Api.PaymentView do
       updated_by: payment.updated_by,
       inserted_at: payment.inserted_at,
       updated_at: payment.updated_at,
+      payment_status: payment.payment_status,
+      status: render_one(payment.status, PaymentStatusView, "payment_status.json"),
+      payment_type: payment.payment_type,
+      type: render_one(payment.type, PaymentTypeView, "payment_type.json"),
       user: render_one(payment.user, UserView, "user.json"),
       update_by: render_one(payment.update_by, UserView, "user.json")
     }
