c214e59d2318b7e635e6cac9f66e610ff7b20e75
laksamanakeris <kerismelayu@gmail.com>
2018-09-29 21:33:09 +0800

    Added relationship

--- lib/bijakhq/payments/payment_type.ex
+++ lib/bijakhq/payments/payment_type.ex
@@ -2,11 +2,14 @@ defmodule Bijakhq.Payments.PaymentType do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Bijakhq.Payments.Payment
 
   schema "payment_types" do
     field :name, :string
     field :description, :string
     field :configuration, :string
+
+    has_many :payments, Payment, foreign_key: :payment_type
 
     timestamps()
   end
