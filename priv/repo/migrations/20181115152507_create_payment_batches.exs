36b7cc15e81e9c54dfb29da7bc7d322c836efc77
laksamanakeris <kerismelayu@gmail.com>
2018-11-15 23:51:47 +0800

    Modified and added new fields

--- priv/repo/migrations/20181115152507_create_payment_batches.exs
+++ priv/repo/migrations/20181115152507_create_payment_batches.exs
@@ -3,10 +3,10 @@ defmodule Bijakhq.Repo.Migrations.CreatePaymentBatches do
 
   def change do
     create table(:payment_batches) do
-      add :date_processed, :naive_datetime
-      add :description, :string
+      add :date_processed, :utc_datetime
+      add :description, :text
       add :is_processed, :boolean, default: false, null: false
-
+      add :generated_request, :json
       timestamps()
     end
 
