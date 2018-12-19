a2e8dfd3faa7aadbfa5325c7d6af52d2062d2c6f
laksamanakeris <kerismelayu@gmail.com>
2018-12-19 11:55:36 +0800

    Disable logger info for prod

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :bijakhq, BijakhqWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :warn
 
 # ## SSL Support
 #
