376ef49b511bc924db58a2e4f0b24ea4bfa9de10
laksamanakeris <kerismelayu@gmail.com>
2019-01-05 23:02:09 +0800

    cleaned up

--- lib/bijakhq_web/views/api/user_view.ex
+++ lib/bijakhq_web/views/api/user_view.ex
@@ -6,11 +6,6 @@ defmodule BijakhqWeb.Api.UserView do
     %{data: render_many(users, UserView, "user.json")}
   end
 
-  # Game result
-  def render("game_result_index.json", %{user: user}) do
-    %{data: render_many(user, UserView,"game_result_user.json")}
-  end
-
   def render("show.json", %{user: user}) do
     %{data: render_one(user, UserView, "user.json")}
   end
@@ -51,16 +46,6 @@ defmodule BijakhqWeb.Api.UserView do
         ranking: weekly.rank,
         amounts: weekly.amounts
       },
-    }
-  end
-
-  def render("game_result_user.json", %{user: user}) do
-    %{
-      user_id: user.id,
-      # email: user.email,
-      username: user.username,
-      profile_picture: UserView.check_profile_picture(user.profile_picture),
-      amounts: user.amounts
     }
   end
 
