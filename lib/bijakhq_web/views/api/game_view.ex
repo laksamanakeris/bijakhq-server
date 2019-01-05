0ff659b7ba521a36641147b34a7e076bd517aac0
laksamanakeris <kerismelayu@gmail.com>
2019-01-05 22:58:51 +0800

    Organize views for game result

---
+++ lib/bijakhq_web/views/api/game_view.ex
@@ -0,0 +1,27 @@
+defmodule BijakhqWeb.Api.GameView do
+  use BijakhqWeb, :view
+  alias BijakhqWeb.Api.GameView
+
+  # Game result
+  def render("game_result_index.json", %{user: user}) do
+    %{data: render_many(user, GameView,"game_result_user.json")}
+  end
+
+  def render("game_result_user.json", %{user: user}) do
+    %{
+      user_id: user.id,
+      username: user.username,
+      profile_picture: GameView.check_profile_picture(user.profile_picture),
+      amounts: user.amounts
+    }
+  end
+
+  def check_profile_picture(nil) do
+    "https://storage.googleapis.com/bijakhq_avatars/uploads/user/avatars/avatar_circle_blue_512dp.png"
+  end
+
+  def check_profile_picture(file) do
+    "https://storage.googleapis.com/bijakhq_avatars/uploads/user/avatars/#{file.file_name}"
+  end
+
+end
