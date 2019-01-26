5b2867855cafa88a88ddcb9c8e98b2ba72338087
laksamanakeris <kerismelayu@gmail.com>
2019-01-26 23:13:19 +0800

    Added semaphore guard for socket and channel
    This to prevent massive burst when high volume users coming to the app

--- lib/bijakhq_web/channels/user_socket.ex
+++ lib/bijakhq_web/channels/user_socket.ex
@@ -6,6 +6,9 @@ defmodule BijakhqWeb.UserSocket do
 
   @max_age 365 * 24 * 60 * 60
 
+  @semaphore_name :semaphore_socketconn
+  @semaphore_max 1000
+
   ## Channels
   channel "game_session:*", BijakhqWeb.GameSessionChannel
   channel "load_test:lobby", BijakhqWeb.LoadTestChannel
@@ -17,20 +20,35 @@ defmodule BijakhqWeb.UserSocket do
     #IO.puts "=============================================================================================================="
     #IO.puts "Connection attempt  > #{token}"
     #IO.puts "=============================================================================================================="
-    case Phauxth.Token.verify(socket, token, @max_age) do
-      {:ok, user_id} ->
-        socket = assign(socket, :user, %{id: user_id})
-        # Logger.warn "SOCKET connected :: id:#{user_id} - time:#{DateTime.utc_now}"
-        {:ok, socket}
-      {:error, _something} ->
-        #IO.inspect something
-        :error
+    if Semaphore.acquire(@semaphore_name, @semaphore_max) do
+      case Phauxth.Token.verify(socket, token, @max_age) do
+        {:ok, user_id} ->
+          socket = assign(socket, :user, %{id: user_id})
+          # Logger.warn "SOCKET connected :: id:#{user_id} - time:#{DateTime.utc_now}"
+          Semaphore.release(@semaphore_name)
+          {:ok, socket}
+        {:error, something} ->
+          #IO.inspect something
+          Logger.warn "SOCKET connect :: error - token #{something} - time:#{DateTime.utc_now} - #{Semaphore.count(@semaphore_name)}"
+          Semaphore.release(@semaphore_name)
+          :error
+      end
+    else
+      Logger.warn "SOCKET connect :: error - too many caller - time:#{DateTime.utc_now} - #{Semaphore.count(@semaphore_name)}"
+      :error
     end
   end
 
   # Use this to connect
   def connect(_params, socket) do
-    {:ok, socket}
+    if Semaphore.acquire(@semaphore_name, @semaphore_max) do
+      Semaphore.release(@semaphore_name)
+      {:ok, socket}
+    else
+      Logger.warn "SOCKET connect :: too many caller - time:#{DateTime.utc_now} - #{Semaphore.count(@semaphore_name)}"
+      Semaphore.release(@semaphore_name)
+      :error
+    end
   end
 
   def id(_socket), do: nil
