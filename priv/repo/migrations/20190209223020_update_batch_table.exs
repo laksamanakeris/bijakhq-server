377c98160b2b75a9bfdff720e40ea460b3a01446
laksamanakeris <kerismelayu@gmail.com>
2019-02-10 16:24:16 +0800

    Add additional fields

--- priv/repo/migrations/20190209223020_update_batch_table.exs
+++ priv/repo/migrations/20190209223020_update_batch_table.exs
@@ -5,6 +5,8 @@ defmodule Bijakhq.Repo.Migrations.UpdateBatchTable do
     alter table(:payment_batches) do
       add :payout_batch_id, :string
       add :batch_status, :string
+      add :amount, :float
+      add :fees, :float
     end
   end
 end
